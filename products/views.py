0942912b6a417d1c968a5730526477e79e05fa51
Nicholas Ladjevich <zantladjevich@gmail.com>
2022-10-11 23:23:42 -0400

    Created post request method

--- products/views.py
+++ products/views.py
@@ -1,15 +1,24 @@
 from itertools import product
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+
+
 from .serializers import ProductsSerializer
 from .models import Products
 from products import serializers
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def products_list(request):
 
-    product = Products.objects.all()
-    serializer = ProductsSerializer(product, many=True)
+    if request.method == 'GET':
+        product = Products.objects.all()
+        serializer = ProductsSerializer(product, many=True)
+        return Response(serializer.data)
 
-    return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, stuatus=status.HTTP_201_CREATED)
