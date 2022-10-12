c777376e2ef44e9a3092ff035bea14d563ef7ae1
Nicholas Ladjevich <zantladjevich@gmail.com>
2022-10-11 20:24:04 -0400

    Created Products serializer

---
+++ products/serializers.py
@@ -0,0 +1,8 @@
+import imp
+from rest_framework import serializers
+from .models import Products
+
+class ProductsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Products
+        fields = ['id', 'title', 'description', 'price', 'inventory_quantity']
